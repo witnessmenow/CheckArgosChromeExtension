b838982ed73bad3c100c705b33dc94735fd29a37
Brian Lough <ecenuig@gmail.com>
2014-01-10 00:02:05 +0000

    Initial work on extension
    Currently replaces all links with product ids to checkargos links

---
+++ checkForLinks.js
@@ -0,0 +1,77 @@
+function getAllArgosLinks()
+{
+	var linksFound = false;
+	var links = document.getElementsByTagName("a");
+	for(var i=0; i<links.length; i++) {
+		if(links[i].href.indexOf("argos.ie") != -1)
+		{
+			linksFound = true;
+			processArgosLinks(links[i]);
+		}
+	}
+	
+	if(linksFound)
+	{
+		//Cant use chrome.tabs here
+		//displayBrowserAction();
+	}
+}
+
+function displayBrowserAction()
+{
+	chrome.tabs.getCurrent(function(tab){
+		
+		chrome.browserAction.setTitle({title : 'Argos links were replaced on this page', tabId: tab.id});
+		chrome.browserAction.setIcon({path: 'Logo38.png', tabId: tab.id});
+	})
+}
+
+function processArgosLinks(linkElement)
+{
+	//Any links that get here are argos.ie links
+	var partNumberRegex = /partNumber\/([0-9]{7})/g;
+	var match = partNumberRegex.exec(linkElement.href);
+	
+	if(match != null && match.length > 1)
+	{
+		//Got a product number!
+		replaceLinkElement(linkElement, generateCheckArgosProductIdLink(match[1]));
+		return;
+	}
+	else
+	{
+		//no product number in the url
+		//unfortunatly doesn't mean its not a valid url!
+		//Argos return a product page if you run a search query
+		//that only returns one item
+		//need to extend checkargos to handle this
+		
+		var searchTermRegex = /searchTerms=([^&]*)&/g;
+		match = searchTermRegex.exec(linkElement.href);
+		if(match != null && match.length > 1)
+		{
+			console.log("CheckArgos currently cant handle this link");
+			return;
+		}
+	}
+	
+	//If we get here it was an argos link that didnt match anything
+	
+}
+
+function replaceLinkElement(currentElement, newUrl)
+{
+	var newLink = document.createElement("a");
+	newLink.href = newUrl;
+	newLink.innerHTML = currentElement.innerHTML;
+	newLink.target = currentElement.target;
+	//replace old element with new one
+	currentElement.parentNode.replaceChild(newLink, currentElement); 
+}
+
+function generateCheckArgosProductIdLink(productId)
+{
+	return 'http://www.checkargos.com/StockCheckPage.php?productId=' + productId;
+}
+
+getAllArgosLinks();
